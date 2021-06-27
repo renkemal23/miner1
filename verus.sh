42bca1a5e7c5df8756d9b6f275aac2cdd2598d9c
renkemal23 <84149346+renkemal23@users.noreply.github.com>
2021-06-26 18:17:11 -0600

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+scrib hellminer veruscoin
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJDGAbfPGUzVMy6BZmMPbqSMiaMeXJbTfs.Kemal23 -p x --cpu 4
